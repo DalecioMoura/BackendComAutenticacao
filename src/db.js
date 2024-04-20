22f20f233086736d36e6d05bf48a31ee729c2c58
DalecioMoura <daleciosantos2012@gmail.com>
2024-04-20 00:41:50 -0300

    Atualização BD

--- src/db.js
+++ src/db.js
@@ -1,11 +1,11 @@
 const pgp = require('pg-promise')();
 
 const db = pgp({
-    user: 'dbapipostgres',//process.env.DB_USER,
-    password: 'MHGQW4vWUBJ700E8eUeeoRMM68hhZVAu',//process.env.DB_PASS,
-    host: 'dpg-cmf0d9acn0vc73buhd50-a',//process.env.DB_HOST,
+    user: 'dbapicontroledematerial_ec9u_user',//process.env.DB_USER,
+    password: 'LoFyqQ3fHsR8cdmPVMym5NdnVppUfmpu',//process.env.DB_PASS,
+    host: 'dpg-codrgigl5elc73ft9ub0-a',//process.env.DB_HOST,
     port: '5432',//process.env.PORT,
-    database: 'dbapicontroledematerial_onp1'//process.env.DB_NAME
+    database: 'dbapicontroledematerial_ec9u'//process.env.DB_NAME
 });
 
 module.exports = db;
