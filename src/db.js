2860aa739b0a2eceeb2a0c4ac50c937e3da5737a
DalecioMoura <daleciosantos2012@gmail.com>
2025-09-05 18:00:30 -0300

    Atualização BD

--- src/db.js
+++ src/db.js
@@ -3,7 +3,7 @@ const pgp = require('pg-promise')();
 const db = pgp({
     user: 'dbapicontroledematerial_z95k_user',//process.env.DB_USER,
     password: 'pGfCLkmeyqIQ1mmcMGlwDVt8U9ERTjD0',//process.env.DB_PASS,
-    host: 'dpg-d2ti3l3uibrs73equvo0-a',//process.env.DB_HOST,
+    host: 'dpg-d2ti3l3uibrs73equvo0-a.oregon-postgres.render.com',//process.env.DB_HOST,
     port: '5432',//process.env.PORT,
     database: 'dbapicontroledematerial_z95k'//process.env.DB_NAME
 });
